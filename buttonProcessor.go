7441657dad78bd5fb8b2ef243deacb9bbb6e34a8
Daniel I. Applebaum <danapple@danapple.com>
2025-04-21 11:14:52 +0200

    Add LED state restoration when reconnected to ButtonBox

--- buttonProcessor.go
+++ buttonProcessor.go
@@ -6,11 +6,23 @@ import (
 	"sync"
 )
 
-var states = make([]bool, 100)
+var buttonStates = make([]bool, 100)
+
+func initLeds() {
+	for _, buttonPinNumber := range BUTTONS {
+		led := convertButtonToLed(buttonPinNumber)
+		if !buttonStates[buttonPinNumber] {
+			led |= 0x80
+		}
+		outChan <- led
+	}
+}
 
 func buttonProcessor(inChan <-chan byte, outChan chan<- byte, wg *sync.WaitGroup, ctx context.Context) {
 	defer wg.Done()
 	log.Printf("buttonProcessor starting\n")
+
+	initLeds()
 
 	for {
 		select {
@@ -33,27 +45,37 @@ func buttonProcessor(inChan <-chan byte, outChan chan<- byte, wg *sync.WaitGroup
 					continue
 				}
 
-				switch button {
-				case WHITE_BUTTON:
-					led = WHITE_BUTTON_LED
-				case GREEN_BUTTON:
-					led = GREEN_BUTTON_LED
-				case YELLOW_BUTTON:
-					led = YELLOW_BUTTON_LED
-				case RED_BUTTON:
-					led = RED_BUTTON_LED
-				case SQUARE_BUTTON:
-					led = SQUARE_BUTTON_LED
-				default:
+				led = convertButtonToLed(button)
+				if led == 0 {
 					continue
 				}
-				newState := !states[button]
+				newState := !buttonStates[button]
 				if !newState {
 					led |= 0x80
 				}
-				states[button] = newState
+				buttonStates[button] = newState
 				outChan <- led
 			}
 		}
 	}
+}
+
+func convertButtonToLed(button byte) byte {
+	var led byte = 0
+
+	switch button {
+	case WHITE_BUTTON:
+		led = WHITE_BUTTON_LED
+	case GREEN_BUTTON:
+		led = GREEN_BUTTON_LED
+	case YELLOW_BUTTON:
+		led = YELLOW_BUTTON_LED
+	case RED_BUTTON:
+		led = RED_BUTTON_LED
+	case SQUARE_BUTTON:
+		led = SQUARE_BUTTON_LED
+	default:
+		led = 0
+	}
+	return led
 }
